865483af31b721888ed427b1977d8e05d531e208
Meiring47817909 <47817909@mynwu.ac.za>
2025-02-06 10:46:15 +0200

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,9 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; } //By convetion entity framework uses this Name of table in database will be Users
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,7 @@
+namespace API.Entities;
+
+public class AppUser
+{
+    public int Id { get; set; } //Default is 0 By convetion entity Framework will use property called id a primary key
+    public required string UserName { get; set; } //Default is null if not required
+}
