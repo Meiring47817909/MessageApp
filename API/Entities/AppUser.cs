8029adfe6f5cec3f522a43b6dba1037b2c2866f8
Meiring47817909 <47817909@mynwu.ac.za>
2025-02-11 18:57:21 +0200

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,11 @@
+using Humanizer.Bytes;
+
 namespace API.Entities;
 
 public class AppUser
 {
     public int Id { get; set; } //Default is 0 By convetion entity Framework will use property called id a primary key
     public required string UserName { get; set; } //Default is null if not required
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }
